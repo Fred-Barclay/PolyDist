4af0e3776877ec3823a456f43f883f24ecbaba21
Fred-Barclay <Fred-Barclay@users.noreply.github.com>
2016-04-24 08:06:16 +1000

    RC 0.2.3

--- PolyDist.py
+++ PolyDist.py
@@ -1,38 +1,62 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-# Begun on Wed May 27 11:01:28 2015
-
-########################################################################
-# This code (PolyDist) was written by Fred Barclay.                    #
-# You may:                                                             #
-# (a). Use this code as you see fit.                                   #
-# (b). Modify this code as needed.                                     #
-# (c). Redistribute copies, whether modified or unchanged, freely.     #
-#                                                                      #
-# You may not:                                                         #
-# (a). Change the text within this comment box or modify the box in    #
-#      any way (this also means you may not remove the box.)           #
-# (b). Claim original authorship. Any additional authors or modifiers  #
-#      may be named as such.                                           #
-#                                                                      #
-# Usage of this code is at your own risk. Notwithstanding any other    #
-# guarantees in any part of the code, the author of the original code, #
-# known as Fred Barclay, is and shall be in no way responsible for any #
-# damages arising from the use of this code. By using this code, you   #
-# accept these terms and all responsibilities.                         #
-########################################################################
-
-# PolyDist.py
+#
+# PolyDist.py (Multi-point distance finder)
+# Copyright (C) 2015,2016 Fred Barclay (https://github.com/Fred-Barclay);
+# (BugsAteFred@gmail.com)
+#
+# Dual-licensed under the GPL v2 and custom license terms.
+# GPL v2:
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation; either version 2
+#   of the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Custom license terms:
+#   You are hereby granted a perpetual, irrevocable license to copy, modify,
+#   publish, release, and distribute this program as you see fit. However,
+#   under no circumstances may you claim original authorship of this program;
+#   you must credit the original author(s). You may not remove the listing of
+#   the original author(s) from the source code, though you may change the
+#   licensing terms. If you publish or release binaries or similarly compiled
+#   files, you must credit the author(s) on your home and/or distribution page,
+#   whichever applies. In your documentation, you must credit the author(s) for
+#   the portions of their code you have used. This, of course, does not revoke
+#   or change your right to claim original authorship to any portions of the
+#   code that you have written.
+#
+#   You must agree to assume all liability for your use of the program, and to
+#   indemnify and hold harmless the author(s) of this program from any liability
+#   arising from use of this program, including, but not limited to: loss of
+#   data, death, dismemberment, or injury, and all consequential and
+#   inconsequential damages.
+#
+#   For clarification, contact Fred Barclay:
+#       https://github.com/Fred-Barclay
+#       BugsAteFred@gmail.com
+#
+# Written in Python 3.
+#
+# Please email all complaints, comments, suggestions, questions, or bugs to
+# BugsAteFred@gmail.com or contact me through GitHub.
+#
+# Dependencies are shapely, matplotlib, and xlsxwriter. You can get by with only
+# shapely if you either:
+#   (a). Answer "N" when prompted to graph and export to a spreadsheet (default
+#        option) and comment out lines 63-64, or
+#   (b). Comment out lines 63-64 and 135-189.
+#
 # Version 0.2.3 RC
 #
-#
-# This code was written in Python 3.
-#
-# Please email all complaints, comments, suggestions, or bugs to
-# BugsAteFred@gmail.com or contact me through github.
-#
-# Dependencies are shapely, matplotlib, and xlsxwriter.
 from shapely import affinity
 from shapely.geometry import LinearRing, LineString, Point
 from ast import literal_eval
@@ -73,91 +97,91 @@ intersections = []
 
 for a in range(len(rot_sequence)):
 
-   # Rotate "ray_main" around the point every 'deg' degrees.
-   ray_rot = affinity.rotate(ray_main, rot_sequence[a], pt)
+#	Rotate "ray_main" around the point every 'deg' degrees.
+	ray_rot = affinity.rotate(ray_main, rot_sequence[a], pt)
 
-   # Find the intersection(s) of 'ray_rot' with the polygon.
-   inter = poly.intersection(ray_rot)
+#	Find the intersection(s) of 'ray_rot' with the polygon.
+	inter = poly.intersection(ray_rot)
 
-   if inter.is_empty:
-       print('No Intersection found for ray', a)
+	if inter.is_empty:
+	print('No Intersection found for ray', a)
 
-   # If there are more than 1 intersections.
-   elif inter.geom_type.startswith('Multi') \
-       or inter.geom_type == 'GeometryCollection':
+#	If there are more than 1 intersections.
+	elif inter.geom_type.startswith('Multi') \
+	or inter.geom_type == 'GeometryCollection':
 
-       # If needed, group the distances together by
-       # "ray_rot". See 'for' statement below:
-       multidist = []
+#		If needed, group the distances together by
+#		`ray_rot`. See 'for' statement below:
+		multidist = []
 
-       # Find the distance from the point to each
-       # intersection along 'ray_rot'.
-       for multipt in inter:
-           dist = point.distance(multipt)
-           multidist.append(dist)
-           intersections.append(list(multipt.coords))
-       print('Multiple Intersections along ray',a)
-       Distance.append(multidist)
+#		Find the distance from the point to each
+#		intersection along 'ray_rot'.
+		for multipt in inter:
+			dist = point.distance(multipt)
+			multidist.append(dist)
+			intersections.append(list(multipt.coords))
+		print('Multiple Intersections along ray',a)
+		Distance.append(multidist)
 
-   # If there is only 1 intersection.
-   else:
-       dist = point.distance(inter)
-       intersections.append(list(inter.coords))
-       Distance.append(dist)
+#	If there is only 1 intersection.
+	else:
+		dist = point.distance(inter)
+		intersections.append(list(inter.coords))
+		Distance.append(dist)
 
 print('Distance =',Distance)
 
 
 #++++++++++++Export to Excel/Calc
 
-xL = input('\nExport data to .xlsx file? y/N ')
+xL = input('\nExport data to .xlsx spreadsheet? y/N ')
 
 if xL == 'y' or xL == 'Y':
 
-    workbook = xlsxwriter.Workbook('PolyDist.xlsx')
-    worksheet = workbook.add_worksheet()
+	workbook = xlsxwriter.Workbook('PolyDist.xlsx')
+	worksheet = workbook.add_worksheet()
 
-    for s in range(len(Distance)):
-        try:
-            if len(Distance[s]) != 1:
-                for t in range(len(Distance[s])):
-                    worksheet.write( s, t, Distance[s][t])
-        except TypeError:
-            worksheet.write( s, 0, Distance[s])
+	for s in range(len(Distance)):
+		try:
+			if len(Distance[s]) != 1:
+				for t in range(len(Distance[s])):
+				worksheet.write( s, t, Distance[s][t])
+		except TypeError:
+			worksheet.write( s, 0, Distance[s])
 
-    workbook.close()
-    print('Data exported sucessfully!')
+	workbook.close()
+	print('Data exported sucessfully!')
 
 #++++++++++++++++Plotting Segment!
 u_plot = input('\nPlot figure on unit graph? y/N ')
 
 if u_plot == 'y' or u_plot == 'Y':
 
-    # Vertices
-    plt.plot([f[0] for f in list(poly.coords)],
-             [f[1] for f in list(poly.coords)],'b*-' )
+#	Vertices
+	plt.plot([f[0] for f in list(poly.coords)],
+			[f[1] for f in list(poly.coords)],'b*-' )
 
-    # "...and so my Main Point is this:"
-    plt.plot(pt[0], pt[1], 'ro')
+#	"...and so my Main Point is this:"
+	plt.plot(pt[0], pt[1], 'ro')
 
-    #
+#	Intersections
+	plt.plot([g[0][0] for g in intersections], [g[0][1] for g in
+		intersections],'g^' )
 
-    # Intersections
-    plt.plot([g[0][0] for g in intersections], [g[0][1] for g in
-        intersections],'g^' )
+#	Rays
+	for h in range(len(intersections)):
+		plt.plot([ pt[0], intersections[h][0][0]], [pt[1],
+			intersections[h][0][1]], 'r--')
 
-    # Rays
-    for h in range(len(intersections)):
-        plt.plot([ pt[0], intersections[h][0][0]], [pt[1],
-          intersections[h][0][1]], 'r--')
+#	Make the coordinate plane 3 units larger than the polygon.
+	plt.axis([min(i[0] for i in vert) - 3, max(i[0] for i in vert)
+		+ 3, min(i[1] for i in vert) - 3, max(i[1] for i in vert) +
+		3 ])
 
-    # Make the coordinate plane 3 units larger than the polygon.
-    plt.axis([min(i[0] for i in vert) - 3, max(i[0] for i in vert)
-        + 3, min(i[1] for i in vert) - 3, max(i[1] for i in vert) +
-        3 ])
+#	Grid
+	plt.grid()
 
-    # Grid
-    plt.grid()
+#	two for the Show
+	plt.show()
 
-    # two for the Show
-    plt.show()
+	print('Figure graphed sucessfully!')
